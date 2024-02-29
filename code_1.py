30b8aef13a56afaed0f49e41f7dc05eb9f9ba18d
NATALY <156665248+NATA4030@users.noreply.github.com>
2024-02-29 19:48:08 -0300

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,25 @@
+    public static void main(String[] args) {
+        int[] data = new int[10];
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Random Number: " + item);
+        }
+public class RandomDataGenerator {
+    }
+
+            data[i] = (int) (Math.random() * 100) + 1;
+        }
+        for (int item : data) {
+}
+            System.out.println("Random Number: " + item);
+    }
+        }
+
+        for (int item : data) {
+        for (int i = 0; i < 10; i++) {
+        int[] data = new int[10];
+        }
+public class RandomDataGenerator {
+
+}
+            data[i] = (int) (Math.random() * 100) + 1;
+    public static void main(String[] args) {
