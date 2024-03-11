83e91089af06140bd58c9455ef59ea42d52b2ea2
NATALY <156665248+NATA4030@users.noreply.github.com>
2024-03-11 17:10:36 -0300

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,13 +1,33 @@
-    public static void main(String[] args) {
-        int[] data = new int[10];
-        }
-        for (int i = 0; i < 10; i++) {
+    const data = generateRandomData();
+
     }
+    data.forEach(item => {
+    });
+    for (let i = 0; i < 10; i++) {
+    const data = [];
+    return data;
+function main() {
+
 }
-        for (int item : data) {
-            data[i] = (int) (Math.random() * 100) + 1;
+}
+        data.push(Math.floor(Math.random() * 100) + 1);
+main();
+function generateRandomData() {
+        console.log("Random Number: " + item);
+    const data = generateRandomData();
+    for (let i = 0; i < 10; i++) {
 
-        }
+main();
 
-public class RandomDataGenerator {
-            System.out.println("Random Number: " + item);
+    const data = [];
+
+        console.log("Random Number: " + item);
+}
+}
+    data.forEach(item => {
+function generateRandomData() {
+    });
+    }
+        data.push(Math.floor(Math.random() * 100) + 1);
+function main() {
+    return data;
