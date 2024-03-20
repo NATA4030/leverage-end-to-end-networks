a37c4f6f795c82d6a2993298b8d2226e76bbce11
NATALY <156665248+NATA4030@users.noreply.github.com>
2024-03-20 17:31:47 -0300

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,12 +1,33 @@
-public class RandomDataGenerator {
-        }
-    public static void main(String[] args) {
+    const data = generateRandomData();
+    for (let i = 0; i < 10; i++) {
 }
+        console.log("Random Number: " + item);
+function main() {
 
     }
-        }
-        int[] data = new int[10];
-        for (int i = 0; i < 10; i++) {
-            data[i] = (int) (Math.random() * 100) + 1;
-            System.out.println("Random Number: " + item);
-        for (int item : data) {
+
+    });
+    const data = [];
+    data.forEach(item => {
+    return data;
+
+        data.push(Math.floor(Math.random() * 100) + 1);
+}
+main();
+function generateRandomData() {        console.log("Random Number: " + item);
+    }
+    data.forEach(item => {
+function main() {
+
+    const data = generateRandomData();
+
+    const data = [];
+
+function generateRandomData() {
+main();
+    });
+}
+}
+    for (let i = 0; i < 10; i++) {
+    return data;
+        data.push(Math.floor(Math.random() * 100) + 1);
