663d23816a3aba00a0e3a60ebb631e92e3c6de9b
NATALY <156665248+NATA4030@users.noreply.github.com>
2024-04-01 20:01:07 -0300

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,27 +1,14 @@
+    main()
     return data
+
+import random
+def generate_random_data():
+    data = [random.randint(1, 100) for _ in range(10)]
+    for item in data:
+
     data = generate_random_data()
 
-def generate_random_data():
-def main():
-    for item in data:
-    main()
-    data = [random.randint(1, 100) for _ in range(10)]
-
         print(f"Random Number: {item}")
-if __name__ == "__main__":
-import random
 
 if __name__ == "__main__":
-
-
-
 def main():
-import random
-    return data
-    for item in data:
-    data = [random.randint(1, 100) for _ in range(10)]
-def generate_random_data():
-    main()
-
-        print(f"Random Number: {item}")
-    data = generate_random_data()
